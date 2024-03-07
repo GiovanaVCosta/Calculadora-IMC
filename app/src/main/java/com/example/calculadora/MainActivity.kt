d73cf4c63f78dbc47accb635da472f93b1801f4a
GIOVANA VIEIRA DA COSTA <23111728@dsn127m1242315.cfp127aluno.local>
2024-03-07 16:25:20 -0300

    calculadora

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,267 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calculadora.ui.theme.CalculadoraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculadoraTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth(),
+
+        color = Color.White
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .background(Color(0xFFED145B))
+                    .fillMaxWidth()
+                    .height(200.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    modifier = Modifier
+                        .width(70.dp)
+                        .height(70.dp),
+                    painter = painterResource(id = R.drawable.rectangle41),
+                    contentDescription = ""
+                )
+
+                Text(
+                    text = "Calculadora  IMC",
+                    color = Color.White,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .offset(y = -30.dp)
+                    .background(
+                        Color(0xFFF0ECEC),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .height(400.dp)
+                    .width(300.dp),
+
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+
+
+                    Text(
+
+                        text = "Seus dados",
+                        color = Color(0XFFEB1450),
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        textAlign = TextAlign.Center
+                    )
+                    Column(verticalArrangement = Arrangement.SpaceEvenly) {
+
+                        Text(
+
+                            text = "Seu peso:",
+                            modifier = Modifier
+                                .padding(top = 5.dp),
+                            color = Color(0xFFEB1450)
+
+
+                        )
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            label = {
+                                Text(
+                                    text = "Seu peso em Kg",
+                                    color = Color.Gray
+                                )
+
+                            },
+                            colors = OutlinedTextFieldDefaults.colors(
+                                unfocusedBorderColor = Color(0xFFEB1450),
+                                focusedBorderColor = Color(0xFFEB1450)
+                            ),
+                            modifier = Modifier
+                                .padding(
+                                    bottom = 10.dp
+                                )
+                        )
+                        Text(
+                            text = "Sua altura:",
+                            modifier = Modifier
+                                .padding(
+                                    bottom = 10.dp
+                                ),
+                            color = Color(0xFFEB1450),
+                        )
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            label = {
+                                Text(
+                                    text = "Sua altura em cm",
+                                    color = Color.Gray
+                                )
+                            },
+                            colors = OutlinedTextFieldDefaults.colors(
+                                unfocusedBorderColor = Color(0xFFEB1450),
+                                focusedBorderColor = Color(0xFFEB1450)
+                            )
+                        )
+
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults
+                            .buttonColors(
+                                containerColor = Color(0xffeb1450)
+                            ),
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .padding(top = 32.dp)
+                            .height(50.dp)
+                            .width(240.dp)
+                    )
+                    {
+                        Text(
+                            text = "Calcular",
+                            fontSize = 20.sp,
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+            Column {
+
+
+                Card(
+                    modifier = Modifier
+                        .size(
+                            width = 300.dp,
+                            height = 100.dp
+                        ),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = CardDefaults.cardColors(
+                        Color(0xaa2d9560)
+                    )
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Column(
+                            modifier = Modifier,
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+
+                        ) {
+                            Text(
+                                modifier = Modifier,
+                                text = "Resultado",
+                                color = Color.White
+                                )
+                            Text(
+                                text = "Peso Ideal",
+                                color = Color.White,
+                                fontSize = 24.sp
+                            )
+                        }
+                        Text(
+                            text ="21.3",
+                            color = Color.White,
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold)
+                    }
+                }
+
+            }
+
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CalculadoraTheme {
+        Greeting()
+    }
+}
